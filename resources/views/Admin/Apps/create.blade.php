d780dd0aa2654fe1dc2f39cceff9115fe3526bc0
Priatmoko <priatmoko@orix.co.id>
2019-05-08 16:01:42 +0700

    Add create Apps menu

--- resources/views/Admin/Apps/create.blade.php
+++ resources/views/Admin/Apps/create.blade.php
@@ -11,8 +11,8 @@
                 ['title'=>'Application / Menu Configuration'])
                 <p class="text-muted">General settings such as name, photo profile, etc</p>
                 <div class="form-group row align-items-center">
-                    <label for="site-title" class="form-control-label col-md-3">
-                        ID / Username
+                    <label for="site-title" class="form-control-label text-md-right col-md-3">
+                        Name / ID
                         <a href="javascript:;"
                             data-html="true" 
                             data-toggle="popover" 
@@ -23,27 +23,72 @@
                             <i class="far fa-question-circle"></i>
                             </a>
                     </label>
-                    <div class="col-md-3">
-                        <input type="text" id="id" name="id"  
-                            class="form-control {{ $errors->has('id') ? ' is-invalid' : '' }} text-center" 
-                            required value="{{\Auth::user()->id}}"  tabindex=1 readonly/>
+                    <div class="col-md-7">
+                        <input type="text" id="name" name="name"  
+                            class="form-control {{ $errors->has('id') ? ' is-invalid' : '' }}" 
+                            required  tabindex=1/>
                         <div class="invalid-feedback">
-                            @if ($errors->has('id'))
-                                {{ $errors->first('id') }}</strong>
+                            @if ($errors->has('name'))
+                                {{ $errors->first('name') }}</strong>
                             @else
-                                {{__('Please fill in your username')}}
+                                {{__('Please fill in application / menu name')}}
                             @endif
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <input type="text" id="username" name="username"  
-                            class="form-control {{ $errors->has('username') ? ' is-invalid' : '' }}" 
-                            required value="{{\Auth::user()->username}}"  tabindex=2 readonly/>
+                    <div class="col-md-1">
+                        <input type="text" id="id" name="id"  
+                            class="form-control {{ $errors->has('id') ? ' is-invalid' : '' }}" readonly/>
                         <div class="invalid-feedback">
-                            @if ($errors->has('username'))
-                                {{ $errors->first('username') }}</strong>
-                            @else
-                                {{__('Please fill in your username')}}
+                            @if ($errors->has('id'))
+                                {{ $errors->first('id') }}</strong>
+                            @endif
+                        </div>
+                    </div>
+                </div>    
+                <div class="form-group row align-items-center">
+                    <label for="parent" class="form-control-label text-md-right col-md-3">
+                        Menu Parent
+                        <a href="javascript:;"
+                            data-html="true" 
+                            data-toggle="popover" 
+                            data-trigger="focus" 
+                            data-content="
+                                <b>Username</b>, it is your username. You can sign in using this username beside email <br/> 
+                                <b>Database info</b> : Users.username">
+                            <i class="far fa-question-circle"></i>
+                            </a>
+                    </label>
+                    <div class="col-md-8">
+                        <input type="text" id="parent" name="parent"  
+                            class="form-control {{ $errors->has('parent') ? ' is-invalid' : '' }}" 
+                            required  tabindex=1/>
+                        <div class="invalid-feedback">
+                            @if ($errors->has('parent'))
+                                {{ $errors->first('parent') }}</strong>
+                            @endif
+                        </div>
+                    </div>
+                </div>    
+                <div class="form-group row align-items-center">
+                    <label for="parent" class="form-control-label text-md-right col-md-3">
+                        Node Name
+                        <a href="javascript:;"
+                            data-html="true" 
+                            data-toggle="popover" 
+                            data-trigger="focus" 
+                            data-content="
+                                <b>Username</b>, it is your username. You can sign in using this username beside email <br/> 
+                                <b>Database info</b> : Users.username">
+                            <i class="far fa-question-circle"></i>
+                            </a>
+                    </label>
+                    <div class="col-md-8">
+                        <input type="text" id="parent" name="parent"  
+                            class="form-control {{ $errors->has('parent') ? ' is-invalid' : '' }}" 
+                            required  tabindex=1/>
+                        <div class="invalid-feedback">
+                            @if ($errors->has('parent'))
+                                {{ $errors->first('parent') }}</strong>
                             @endif
                         </div>
                     </div>
