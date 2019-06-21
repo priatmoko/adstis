5ddb40d99efacb1aaf10c7936ad5bd4f7042dc98
Priatmoko <priatmoko@orix.co.id>
2019-06-21 16:58:56 +0700

    add attribute data into datatable

--- public/assets/Admin/Apps/index.js
+++ public/assets/Admin/Apps/index.js
@@ -5,6 +5,7 @@ var conf = {
     "info"      : false,
     "searching" : false
 }
+table = $("#table-apps").DataTable(conf);
 /**
  * Initial event
  */
@@ -50,66 +51,38 @@ var arrangeCol = function(td){
 }
 var injectTable = function(obj){
     
-    var tr = "";
+    table.clear();
     var td = $('#table-apps thead tr th');
     var tbody = $('#table-apps tbody');
-    var column = arrangeCol(td);
-    conf.columns = column;
+    // var column = arrangeCol(td);
+    // conf.columns = column;
     // console.log(table.columns);
     var records = new Array;
     $.each(obj, function(i, v) {
-        var person = new Object();
+        var row = new Array();
         $.each(td, function(index, field) {
             field = $(this).data('field');
             if (field=='icon'){
-                if (v[$(this).data('field')]==null)
-                    person[field] = "<i class='fa fa-th'></i>";
-                else
-                    person[field] = "<i class='fa "+v[$(this).data('field')]+"'></i>";    
+                if (v[$(this).data('field')]==null){
+                    row.push("<i class='fa fa-th'></i>");
+                }else{
+                    row.push("<i class='fa "+v[$(this).data('field')]+"'></i>");
+                }
             }else{
-                person[field] = v[field];
+                row.push(v[field]);
             }
              
         });
-        records.push(person);
-        // datas = "";
-        // $.each(v, function(id, iv) {
-        //     datas += 'data-' + id + '="' + iv + '"';
-        // });
-        // tr += "<tr id='act_" + i + "' " + datas + " class='context-menu' title='Right Click to choose operation. &#013;Menu  : &#013;"+v.name+" - "+v.id+"'>";
-        // $.each(td, function(index, field) {
-        //     tr += "<td style='" + $(this).attr('style') + "' class='" + $(this).attr('class') + "'>";
-        //     if ($(this).data('field')=='icon'){
-        //         if (v[$(this).data('field')]==null)
-        //             tr += "<i class='fa fa-th'></i>";
-        //         else
-        //             tr += "<i class='fa "+v[$(this).data('field')]+"'></i>";        
-        //     }else{
-        //         if (v[$(this).data('field')]==null)
-        //             tr += '-';
-        //         else
-        //             tr += v[$(this).data('field')];
-        //     }
-            
-        //     tr += "</td>";
-        // });
-        // tr += "</tr>";
+        var appendRow = table.row.add(row);
+        var t = appendRow.node();
+        appendRow.draw();
+        datas = "";
+        $.each(v, function(id, iv) {
+            datas += 'data-' + id + '="' + iv + '"';
+            $(t).attr('data-' + id, iv);
+        });
+        $(t).addClass('context-menu');
+        
     });
-    table = $("#table-apps").DataTable(conf);
-    table.clear();
-    table.rows.add(records).draw();
-    // console.log(records);
-    // tbody.html(tr);
-    
-    // $("#table-apps").DataTable({
-    //     "scrollY"   : 400,
-    //     "paging"    : false,
-    //     "ordering"  : false,
-    //     "info"      : false,
-    //     "searching" : false
-    // })
-    // .rows()
-    // .invalidate('data')
-    // .draw();
-    
+    // table.rows.add(records).draw();
 }
